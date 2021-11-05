462c9d53365ab42d110aca6c99bf7999d75b2a70
thulanimbatha <thulanimbatha06@gmail.com>
2021-11-05 08:40:01 +0200

    added readme & main.py file

---
+++ main.py
@@ -0,0 +1,9 @@
+# main pong file
+from turtle import Screen, width
+
+screen =  Screen()
+screen.setup(width=1000, height=600)
+screen.bgcolor("black")
+screen.title("Python Pong")
+
+screen.exitonclick()
