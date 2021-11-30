db903f2e8e6b2f96f03149488e7520e5a166f7ad
thulanimbatha <thulanimbatha06@gmail.com>
2021-11-30 09:26:19 +0200

    added ball class

---
+++ ball.py
@@ -0,0 +1,11 @@
+# ball class
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.speed("fastest")
