39b606ce6e25aa4ec739f9762ba7b9942c5f730c
thulanimbatha <thulanimbatha06@gmail.com>
2021-11-30 08:39:52 +0200

    created screen setup

--- main.py
+++ main.py
@@ -2,4 +2,9 @@
 from turtle import Screen, width
 from paddle import Paddle
 
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Mondeor Pong!🙂")
 
+screen.exitonclick()
