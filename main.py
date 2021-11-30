909886d8ac53cb3bb0406cd9eb69d332e0ca185c
thulanimbatha <thulanimbatha06@gmail.com>
2021-11-30 08:33:45 +0200

    cleared projects

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 # main pong file
 from turtle import Screen, width
-
-screen =  Screen()
-screen.setup(width=1000, height=600)
-screen.bgcolor("black")
-screen.title("Python Pong")
+from paddle import Paddle
 
 
-screen.exitonclick() 
--- paddle.py
+++ paddle.py
@@ -1,1 +1,13 @@
 # paddle class
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    # constructor
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.goto(-500, 0)
+        
